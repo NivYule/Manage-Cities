9c2ba96778a2dc273dc3f9dc896cde564857d4e1
Niv Yulevitch <niv.yulevitch@gmail.com>
2020-07-12 11:42:30 +0300

    a little bit changing

--- index.js
+++ index.js
@@ -11,12 +11,12 @@ addCityBtn.on("click", () => {
   const mainDiv = $("<div></div>")
     .addClass("card")
     .css({ display: "inline-block" });
-  const ImageOfTheCity = $(`<img src="${cityImage.val()}">`).addClass(
-    "card-img-top"
+  const ImageOfTheCity = $(`<img src="${cityImage.val()}">`).css(
+    "width",
+    "250px"
   );
   const cardBody = $("<div></div>").addClass("card-body");
   const nameOfTheCity = $(`<h5>${cityName.val()}</h5>`).addClass("card-title");
-  const p = $(`<p>${cityName.val()} is The Best City Ever !!!</p>`);
   const likeBtn = $(`<input type="button" id="Like">`)
     .addClass("btn btn-info")
     .val("like");
@@ -25,17 +25,14 @@ addCityBtn.on("click", () => {
   mainDiv.append(ImageOfTheCity);
   mainDiv.append(cardBody);
   cardBody.append(nameOfTheCity);
-  cardBody.append(p);
   cardBody.append(likeBtn);
   cardBody.append(LikesCounter);
   cardsContainer.append(mainDiv);
 
   likeBtn.on("click", () => {
     setTimeout(() => {
-      (function plusOne() {
-        numOfLikes++;
-        LikesCounter.text(numOfLikes + "👍");
-      })();
+      numOfLikes++;
+      LikesCounter.text(numOfLikes + "👍");
     }, 2000);
   });
 });
